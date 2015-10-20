59dc946a537d75fe5a0d5413804a6d1b232c2436
Edoardo Negri <edne@gmx.com>
2015-10-20 22:23:13 +0200

    begin

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject biji "0.1.0-SNAPSHOT"
+  :description "A wise IRC bot"
+  :url "http://github.com/edne/biji"
+  :license {:name "GNU Affero General Public License"
+            :url "http://www.gnu.org/licenses/agpl-3.0.en.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :main biji.core)
---
+++ test/biji/core_test.clj
@@ -0,0 +1,7 @@
+(ns biji.core-test
+  (:require [clojure.test :refer :all]
+            [biji.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
